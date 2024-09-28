5528560dd4570772f59dfcb50f3af1fbe969305a
Omar Faruk <omarfaruk20@iut-dhaka.edu>
2024-09-28 12:00:00 +0600

    HR_Function

---
+++ Hackerrank_Cpp/Attribute_Parser.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+#include <string>
+using namespace std;
+
+int main() {
+
+    int a, b;
+    cin >> a >> b;
+
+    string h;
+    string s;
+    for (int i = 0; i < a; i++) {
+        //Take whole line of string input
+        getline(cin, s);
+        cout << s << endl;
+        cout << i << endl;
+    }
+    cout << h;
+
+
+
+
+    return 0;
+
+}
